e342620f4b373f427ffbb2e20f8f046a63b81714
yasuhisa <penmouse275@nifty.com>
2017-01-31 12:30:39 +0000

    path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  resources :poems ,only: [:show, :update, :destroy, :edit, :new, :create]
-  root 'poems#index'
+  resources :poems ,only: [:index, :show, :update, :destroy, :edit, :new, :create]
 end
